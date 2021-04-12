708aff0a3ad67a954e8d788cc9d1b3467d667c6f
Mohsin Siddiqui <siddiquimohsin660@gmail.com>
2021-04-13 00:08:22 +0500

    LAB8 is added

--- src/test/scala/LAB8/EXQT83.scala
+++ src/test/scala/LAB8/EXQT83.scala
@@ -10,7 +10,7 @@ class EXQT83 extends FreeSpec with ChiselScalatestTester {
   "EXQT83" in {
     test(new EXQ83) { c =>
       c.io.addr.poke(3.U)
-      c.clock.step(50)
+      c.clock.step(10)
     }
   }
 }
