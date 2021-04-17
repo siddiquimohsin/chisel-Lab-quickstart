cbbcc81958993a33b92fabb4167e4c6f1c3cb667
Mohsin Siddiqui <siddiquimohsin660@gmail.com>
2021-04-17 14:29:36 +0500

    LAB8 is added

--- src/main/scala/LAB8/EXQ83.scala
+++ src/main/scala/LAB8/EXQ83.scala
@@ -6,12 +6,17 @@ import chisel3.util.experimental.loadMemoryFromFile
 
 class instruction extends Bundle{
     val addr = Input(UInt(32.W))
-    val inst = Output(UInt(32.W))
+    val instwrite = Input(UInt(32.W))
+    val write = Input(Bool())
+    val instread = Output(UInt(32.W))
 }
 
 class EXQ83 extends Module{
     val io = IO(new instruction)
     val mem = Mem(32, UInt(32.W))
-    loadMemoryFromFile(mem, "/home/mohsin/chisel-quickstart/src/test/scala/LAB8/data.txt")
-    io.inst := mem(io.addr)
+    //loadMemoryFromFile(mem, "/home/mohsin/chisel-quickstart/src/test/scala/LAB8/data.txt")
+    when(io.write === true.B){
+        mem.write(io.addr,io.instwrite)
+    }
+    io.instread := mem(io.addr)
 }
--- src/test/scala/LAB8/EXQT83.scala
+++ src/test/scala/LAB8/EXQT83.scala
@@ -10,7 +10,13 @@ class EXQT83 extends FreeSpec with ChiselScalatestTester {
   "EXQT83" in {
     test(new EXQ83) { c =>
       c.io.addr.poke(3.U)
-      c.clock.step(10)
+      c.io.instwrite.poke("h00500193".U)
+      c.io.write.poke(true.B)
+      c.clock.step(2)
+
+      c.io.write.poke(false.B)
+      c.io.addr.poke(3.U)
+      c.clock.step(2)
     }
   }
 }
